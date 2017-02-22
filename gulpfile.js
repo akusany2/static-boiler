ab57303a6c59f8aabc9b1ba91e0b5b08d922039a
akusany2 <ankitkumar0411@gmail.com>
2017-02-22 22:47:33 +0530

    init

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+	var gulp     = require('gulp'),
+	sass         = require('gulp-sass'),
+	plumber      = require('gulp-plumber'),
+	cleanCSS 		 = require('gulp-clean-css'),
+	postcss      = require('gulp-postcss'),
+	autoprefixer = require('gulp-autoprefixer'),
+	rename 			 = require('gulp-rename'),
+	cssMin 			 = require('gulp-cssmin')
+	cssConcat 	 = require('gulp-concat-css');
+
+	var uglify 	 = require('gulp-uglify'),
+	concat 			 = require('gulp-concat');
+
+var sassFiles = 'scss/*.scss';
+var jsFiles   = 'lib/*.js';
+var jsDir 		= './js/*.js';
+var cssDir 		= 'css/*.css';
+
+//pre processor
+gulp.task('sass', function(){
+	gulp.src(sassFiles)
+		.pipe(plumber())
+		.pipe(sass({outputStyle: 'compressed'}))
+		.pipe(autoprefixer({ browsers: ['last 3 versions'], cascade: false }))
+		.pipe(cleanCSS())
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('./css'));
+});
+
+// Concat css
+gulp.task('cssconcat', function(){
+	return gulp.src(cssDir)
+	.pipe(plumber())
+	.pipe(cssConcat('style.css'))
+	.pipe(cssMin())
+	.pipe(gulp.dest('./'));
+});
+
+// Compress and export to js folder
+gulp.task('js', function() {
+  return gulp.src('lib/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/'));
+});
+
+// Concate JS files into app.js
+gulp.task('jsconcat', function(){
+	return gulp.src([
+		'./js/jquery.js',
+		// './js/particles.min.js',
+		// './js/app.js',
+		])
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./builds/'));
+});
+
+
+gulp.task('watch', function(){
+	gulp.watch(sassFiles, ['sass']);
+	gulp.watch(jsFiles, ['js']);
+	gulp.watch(jsDir, ['jsconcat']);
+	gulp.watch(cssDir, ['cssconcat']);
+});
+
+gulp.task('default', ['watch']);
